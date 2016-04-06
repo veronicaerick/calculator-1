2640520a0adca8ccd3ce0350aba1e0f1d2f5f58e
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 12:16:38 -0700

    Made a basic calculator

---
+++ arithmetic.py
@@ -0,0 +1,31 @@
+def add(num1, num2):
+    """Adds two numbers together"""
+    return num1 + num2
+
+def subtract(num1, num2):
+    """Subtracts one number from another"""
+    return num1 - num2
+
+def multiply(num1, num2):
+    """Multiplies two numbers"""
+    return num1 * num2
+
+def divide(num1, num2):
+    """Divides one number from another number"""
+    return num1 / num2
+
+def square(num1):
+    """Squares a number"""
+    return num1 ** 2 
+
+def cube(num1):
+    """Cubes a number"""
+    return num1 ** 3
+
+def power(num1, num2):
+    """Calculates the power of one number using another"""
+    return num1 ** num2
+
+def mod(num1, num2):
+    """Calculates the remainder of one number divided by another"""
+    return num1 % num2
